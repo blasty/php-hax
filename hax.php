9303b3d82cc93216e5e6eee278e03199acd43305
blasty <blasty@fail0verflow.com>
2012-04-07 13:13:03 +0200

    Add php version note and check

--- hax.php
+++ hax.php
@@ -1,4 +1,9 @@
 <?
+	$ver = explode(".", phpversion());
+
+	if (($ver[0] < 5) || ($ver[0] == 5 && $ver[1] < 4))
+		trigger_error("hax.php needs PHP 5.4.0", E_USER_ERROR);
+
 	$libs = glob("lib/*");
 
 	foreach($libs as $lib)
